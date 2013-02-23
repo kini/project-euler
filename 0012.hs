aada0919708acc1cf2b4da7053e1d1ddf2890a1b
Keshav Kini <keshav.kini@gmail.com>
2013-02-22 23:59:19 -0600

    n and n+1 are coprime

--- 0012.hs
+++ 0012.hs
@@ -20,7 +20,16 @@ factorize n = reverse $ factorize' n [] primes
 
 numFactors n = product $ map (+1) $ factorize n
 
-val = head $ dropWhile ((<= 500) . numFactors)
-      [n*(n+1) `div` 2 | n <- [1..]]
+-- since n and n+1 must be relatively prime, so too must n/2 and n+1,
+-- or n and (n+1)/2 (depending on which one is even, as one of them
+-- clearly has to be). Thus the number of factors of their product,
+-- i.e. the n-th triangular number, is equal to the product of their
+-- numbers of factors.
+numFactorsT_i i = product $ map numFactors $
+                  if 2 `divides` i then [i `div` 2, i + 1]
+                                   else [i, (i + 1) `div` 2]
+
+val = let n = head $ dropWhile ((<= 500) . numFactorsT_i) [1..]
+  in n*(n+1) `div` 2 
 
 main = print val
