9bc9bbeeae7cf6139790a79f4959c9dd504465ea
Keshav Kini <keshav.kini@gmail.com>
2013-02-28 22:12:54 -0800

    Return index rather than value

--- 0025.hs
+++ 0025.hs
@@ -4,6 +4,8 @@ import Data.List
 
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
-val = let Just val = find ((>= 1000) . length . show) fibs in val
+val = let
+  Just (val, _) = find ((>= 1000) . length . show . snd) (zip [0..] fibs)
+  in val
 
 main = print val
